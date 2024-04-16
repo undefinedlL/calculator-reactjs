fa0462f495d33367875559a7be1df56815473a1e
Speedwagon10 <105142258+Speedwagon10@users.noreply.github.com>
2024-04-16 12:44:02 +0300

    operations.js file was fixed

--- src/helpers/operations.js
+++ src/helpers/operations.js
@@ -1,42 +1,31 @@
 export const addOperands = (operand_1, operand_2) => {
-    let result = '';
     if (operand_2 !== '') {
-        result = String(Number(operand_1) + Number(operand_2)); 
-    } else {
-        result = operand_1;
-    }   
-    return (result.length < 10 ? result : String(Number(result).toFixed(2)))  
-
-}
-
-export const subtractOperands = (operand_1, operand_2) =>  {
-    let result = '';
-    if (operand_2 !== '') {
-        result = String(Number(operand_1) - Number(operand_2)); 
-        return (result.length < 10 ? result : String(Number(result).toFixed(2)))  
-    } else {
-        return operand_1
-    }
-
-}
-
-export const multiplyOperands = (operand_1, operand_2) => {
-    let result = '';
-    if (operand_2 !== '') {
-        result = String(Number(operand_1) * Number(operand_2)); 
-        return (result.length < 10 ? result : String(Number(result).toFixed(2)))
+        return String(Number(operand_1) + Number(operand_2)); 
     } else {
         return operand_1;
     }   
+}
 
+export const subtractOperands = (operand_1, operand_2) =>  {
+    if (operand_2 !== '') {
+        return String(Number(operand_1) - Number(operand_2)); 
+    } else {
+        return operand_1
+    }
+}
+
+export const multiplyOperands = (operand_1, operand_2) => {
+    if (operand_2 !== '') {
+        return String(Number(operand_1) * Number(operand_2)); 
+    } else {
+        return operand_1;
+    }   
 }
 
 export const divideOperands = (operand_1, operand_2) => {
-    let result = '';
     if (operand_1 !== '0' && operand_2 !== '0') {
         if (operand_2 !== '') {
-            result = String(Number(operand_1) / Number(operand_2));
-            return (result.length < 10 ? result : String(Number(result).toFixed(2)));
+            return String(Number(operand_1) / Number(operand_2));
         } else {
             return operand_1;
         }
